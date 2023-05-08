79aeb0e9cea5d4d717afb261caf1025aa7cfb45e
sushanta902 <hacktech902@gmail.com>
2023-05-08 17:00:46 +0545

    small href

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -38,8 +38,7 @@ console.log(tweets)
     <section class="about-section">
   <h2>About Me : Sushanta Neupane</h2>
   <p>Welcome to our About page! We're delighted to introduce ourselves and share a bit about who we are, what we do, and what we stand for.</p>
-  <h3>Twitter-clone</h3>
-  <p>We are <span class="company-name">[myTwitter]</span>, a dynamic and innovative <span class="company-industry">[industry]</span>  committed to <span class="company-mission">[core mission]</span>.</p>
+  
     </section>
         {tweets.map((element) => (
           <Tweets
@@ -51,6 +50,7 @@ console.log(tweets)
             img={element.image}
             avatar={'66668423'}
             date={element.createdAt}
+            uid={element.user._id}
           />
         ))}
 </div>
